380cb15032482843a296b491c5197fbd3b7c0f36
FBF <francisco.bulhosa@gmail.com>
2023-08-01 15:57:57 +0100

    Code edit

--- Falcon_copy.py
+++ Falcon_copy.py
@@ -1,5 +1,3 @@
-
-
 # pip install langchain==0.0.173
 # pip install chromadb==0.3.23
 # pip install pypdf==3.8.1
@@ -25,16 +23,76 @@ llm = GPT4All(model = LLM_path, verbose=True)
 
 # Pre process the Data from the csv's
 
+
+
+
+
+
+
+
+
+tokenizer = Tokenizer(num_words=100000) # You can adjust the number of words based on your data
+
+# Tokenize the "text" column of your dataframe
+tokenized_df = tokenizer.batch_encode_plus(
+df["text"],
+padding="max_length",
+truncation=True,
+max_length=512,
+return_tensors="pt")
+
+# Create the tokenized dataframe
+tokenized_df = pd.DataFrame(tokenized_df.tolist())
+
+# Use the tokenized dataframe to create the embeddings
+embeddings = HuggingFaceEmbeddings.from_tensor(tokenized_df["input_ids"])
+embeddings = embeddings.to(device)
+
+
+
+
+
+
+
 # Read the CSV file
 file_path = 'C:/Users/franc/Documents/GitHub/LangchainDocuments/pdf_extractions/rgeu.csv'
 df = pd.read_csv(file_path)
 
-# Extract the "Text" column
-texts = df['Text'].tolist()
+# Extract the "Text" column (old method)
+### texts = df['Text'].tolist()
+
+
+
+# Define a custom encoding scheme that maps the "Article" column to a number
+encoding_scheme = HfEncodingScheme(
+   input_ids=["Article"],
+   output_ids=["Text"],
+   num_output_ids=1,
+   padding="max_length",
+   truncation=True,
+   max_length=100,
+   return_tensors="pt"
+)
+
+# Encode the data using the custom encoding scheme
+encoded_data = encoding_scheme.encode_plus(df, return_tensors="pt")
+
+# Create a HuggingFaceEmbeddings model with the encoded data
+model = HfEmbeddings(encoded_data)
+
+# Get the embeddings for the "Article" column
+embeddings = model.get_embeddings("Article")
+
+
+
+
+
+
+
+
 
 # Create embeddings
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
 
 # Store embeddings on chroma
 db = Chroma.from_texts(texts, embeddings, persist_directory="db2")
